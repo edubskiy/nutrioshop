94b30dfb18eaa1f450bd08918e23c0cd43456c35
Evgeniy <edubskiy@openwaygroup.com>
2019-11-08 23:51:55 +0300

    Added provider v3.0.0 package; code refactor; updated gitignore

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:nutrioshop/widgets/product.dart';
+import 'package:nutrioshop/models/product.dart';
 import 'package:nutrioshop/widgets/product_item.dart';
 
 import '../dumm_data.dart';
