f560533342c4c4ec0528cb800914582ae2610238
Evgeniy <edubskiy@openwaygroup.com>
2019-11-14 14:13:25 +0300

    Show snackbar when product is being added to shopping cart

--- lib/providers/cart.dart
+++ lib/providers/cart.dart
@@ -62,4 +62,20 @@ class Cart with ChangeNotifier {
     _items = {};
     notifyListeners();
   }
+
+  void removeSingleItem(productId) {
+    if ( ! _items.containsKey(productId)) return;
+
+    if (_items[productId].quantity > 1) {
+      _items.update(productId, (existingCartItem) => CartItem(
+        id: existingCartItem.id,
+        price: existingCartItem.price,
+        quantity: existingCartItem.quantity - 1,
+        title: existingCartItem.title
+      ));
+    } else {
+      _items.remove(productId);
+    }
+    notifyListeners();
+  }
 }
