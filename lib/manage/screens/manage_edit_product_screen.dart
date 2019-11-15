d513e3011e08799d55d0055564324dae47f66f58
Evgeniy <edubskiy@openwaygroup.com>
2019-11-15 17:14:07 +0300

    Added new input to load image via URL with preview box

--- lib/manage/screens/manage_edit_product_screen.dart
+++ lib/manage/screens/manage_edit_product_screen.dart
@@ -11,13 +11,31 @@ class ManageEditProductScreen extends StatefulWidget {
 class _ManageEditProductScreenState extends State<ManageEditProductScreen> {
   final _priceFocusNode = FocusNode();
   final _descriptionFocusNode = FocusNode();
+  final _imageUrlFocusNode = FocusNode();
+  final _imageUrlController = TextEditingController();
+
+  void initState() {
+    _imageUrlFocusNode.addListener(_updateImageUrl);
+
+    super.initState();
+  }
 
   @override
   void dispose() {
     _priceFocusNode.dispose();
     _descriptionFocusNode.dispose();
+    _imageUrlFocusNode.removeListener(_updateImageUrl);
+    _imageUrlFocusNode.dispose();
     
     super.dispose();
+  }
+
+  _updateImageUrl() {
+    if ( ! _imageUrlFocusNode.hasFocus) {
+      // it is a bit of a hack: force update state to provide text of _imageUrlController
+      // and update our image preview
+      setState(() {});
+    }
   }
 
   @override
@@ -48,11 +66,43 @@ class _ManageEditProductScreenState extends State<ManageEditProductScreen> {
                 },
               ),
               TextFormField(
-                decoration: InputDecoration(labelText: 'Description'),
-                maxLines: 3,
-                focusNode: _descriptionFocusNode,
-                keyboardType: TextInputType.multiline
-              ),
+                  decoration: InputDecoration(labelText: 'Description'),
+                  maxLines: 3,
+                  focusNode: _descriptionFocusNode,
+                  keyboardType: TextInputType.multiline),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: <Widget>[
+                  Container(
+                    width: 100,
+                    height: 100,
+                    margin: EdgeInsets.only(top: 8, right: 10),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        width: 1,
+                        color: Colors.grey
+                      ),
+                    ),
+                    child: _imageUrlController.text.isEmpty 
+                      ? Text('Enter image URL') 
+                      : FittedBox(
+                        child: Image.network(
+                          _imageUrlController.text,
+                          fit: BoxFit.cover
+                        ),
+                      ),
+                  ),
+                  Expanded(
+                    child: TextFormField(
+                      decoration: InputDecoration(labelText: 'Image URL'),
+                      keyboardType: TextInputType.url,
+                      textInputAction: TextInputAction.done,
+                      controller: _imageUrlController,
+                      focusNode: _imageUrlFocusNode,
+                    ),
+                  )
+                ],
+              )
             ],
           ),
         ),
