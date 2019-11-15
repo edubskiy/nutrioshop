10407bf413094df1ba966244fe55104a18a7f625
Evgeniy <edubskiy@openwaygroup.com>
2019-11-15 16:51:42 +0300

    Added more input fields to edit product screen

--- lib/manage/screens/manage_edit_product_screen.dart
+++ lib/manage/screens/manage_edit_product_screen.dart
@@ -9,6 +9,17 @@ class ManageEditProductScreen extends StatefulWidget {
 }
 
 class _ManageEditProductScreenState extends State<ManageEditProductScreen> {
+  final _priceFocusNode = FocusNode();
+  final _descriptionFocusNode = FocusNode();
+
+  @override
+  void dispose() {
+    _priceFocusNode.dispose();
+    _descriptionFocusNode.dispose();
+    
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,7 +34,25 @@ class _ManageEditProductScreenState extends State<ManageEditProductScreen> {
               TextFormField(
                 decoration: InputDecoration(labelText: 'Title'),
                 textInputAction: TextInputAction.next,
-              )
+                onFieldSubmitted: (_) {
+                  FocusScope.of(context).requestFocus(_priceFocusNode);
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Title'),
+                textInputAction: TextInputAction.next,
+                keyboardType: TextInputType.number,
+                focusNode: _priceFocusNode,
+                onFieldSubmitted: (_) {
+                  FocusScope.of(context).requestFocus(_descriptionFocusNode);
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Description'),
+                maxLines: 3,
+                focusNode: _descriptionFocusNode,
+                keyboardType: TextInputType.multiline
+              ),
             ],
           ),
         ),
