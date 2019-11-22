274bf53055c5da7e8d1170b3a21bb57815e679c4
Evgeniy <edubskiy@openwaygroup.com>
2019-11-22 18:55:54 +0300

    Code refactor

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -8,25 +8,24 @@ class Auth with ChangeNotifier {
   DateTime _expiryDate;
   String _userId;
 
-  Future<void> signup(String email, String password) async {
-    const url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDDRH9W5W96qD9oNlY4BH3zdO679ZYlkno';
+  Future<void> _authentificate(String email, String password, String urlSegment) async {
+    final url = 'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyDDRH9W5W96qD9oNlY4BH3zdO679ZYlkno';
+
     final response = await http.post(url, body: json.encode({
       'email': email,
       'password': password,
       'returnSecureToken': true
     }));
-    print('response from signup');
+
+    print('response from $urlSegment');
     print(json.decode(response.body));
   }
 
+  Future<void> signup(String email, String password) async {
+    return _authentificate(email, password, 'signUp');
+  }
+
   Future<void> signin(String email, String password) async {
-    const url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDDRH9W5W96qD9oNlY4BH3zdO679ZYlkno';
-    final response = await http.post(url, body: json.encode({
-      'email': email,
-      'password': password,
-      'returnSecureToken': true
-    }));
-    print('response from signin');
-    print(json.decode(response.body));
+    return _authentificate(email, password, 'signInWithPassword');
   }
 }
