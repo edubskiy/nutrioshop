cf43292e9e14b3d1d96772dc33cd23fed2490694
Evgeniy <edubskiy@openwaygroup.com>
2019-11-22 17:10:38 +0300

    Hook up signin method to corresponding button

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -113,6 +113,10 @@ class _AuthCardState extends State<AuthCard> {
     });
     if (_authMode == AuthMode.Login) {
       // Log user in
+      await Provider.of<Auth>(context, listen: false).signin(
+        _authData['email'], 
+        _authData['password']
+      );
     } else {
       // Sign user up
       await Provider.of<Auth>(context, listen: false).signup(
