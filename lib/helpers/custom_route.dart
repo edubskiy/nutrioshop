393899a072f44e7a09a9d498f7aa46aefcdd967a
Evgeniy <edubskiy@openwaygroup.com>
2019-11-25 23:38:00 +0300

    Changed default page transition for all application routes

--- lib/helpers/custom_route.dart
+++ lib/helpers/custom_route.dart
@@ -20,3 +20,19 @@ class CustomRoute<T> extends MaterialPageRoute<T> {
     // return super.buildTransitions(context, animation, secondaryAnimation, child);
   }
 }
+
+class CustomPageTransitionBuilder extends PageTransitionsBuilder {
+  @override
+  Widget buildTransitions<T> (
+    PageRoute<T> route,
+    BuildContext context,
+    Animation<double> animation,
+    Animation<double> secondaryAnimation,
+    Widget child
+  ) {
+    if (route.settings.isInitialRoute) {
+      return child;
+    }
+    return FadeTransition(opacity: animation, child: child);
+  }
+}
