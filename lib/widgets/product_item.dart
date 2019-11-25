eb2725137d35fad68fc6556817f7a4d982a7e375
Evgeniy <edubskiy@openwaygroup.com>
2019-11-25 19:25:24 +0300

    Fade in products on overview page with the help of placeholder

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -23,10 +23,11 @@ class ProductItem extends StatelessWidget {
                 arguments: product.id
               );
             },
-            child: Image.network(
-              product.imageUrl,
+            child: FadeInImage(
+              placeholder: AssetImage('assets/images/product-placeholder.png'),
+              image: NetworkImage(product.imageUrl),
               fit: BoxFit.cover,
-            ),
+            ) 
           ),
           footer: GridTileBar(
             leading: Consumer<Product>(
